58fa89c438bc8e190d0cdcff65e11339b36d3163
massayphoto-sys <massayphoto@gmail.com>
2026-07-31 02:18:02 -0400

    Config V9 modificado

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = Object.freeze({
   appName: 'Blackjack Trainer',
   version: '9.0.0-auth-foundation',
-  supabaseUrl: 'https://cnrmmmnndxtjhaboqxhil.supabase.co',
+  supabaseUrl: 'https://cnrmmmndxtjhaboqxhil.supabase.co',
   supabasePublishableKey: 'sb_publishable_O1umLqsF6m11SoVS14aN6w_aiyqQw5t',
   requestTimeoutMs: 12000
 });
